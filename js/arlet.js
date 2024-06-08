ba7f0bb2d733cd44c60d721e0fc5f0c001bea7b8
Chamara perera <wchamaraask@gmail.com>
2024-06-09 01:28:40 +0530

    Update arlet.js

--- js/arlet.js
+++ js/arlet.js
@@ -33,18 +33,39 @@ if (stat) {
             message = 'Something went wrong. Please try again.';
             break;
         case 'Already_Exists':
-            message = 'Account already exists.';
+            message = 'Account Already Exists.';
+            break;
+        case 'Email_Exists':
+            message = 'Email Already Exists.';
             break;
         case 'none':
             title = 'Account Creation Saved Successfully.';
             message = 'Welcome to the Library management system';
             icon = 'success';
             break;
+        case 'Created':
+            title = 'User Added Successfully.';
+            icon = 'success';
+            break;
+        case 'Invalid_user_id_format':
+            title = 'Invalid User Id Format';
+            break;
+        case 'User_id_already_exists':
+            title = 'User Id Already Exists';
+            break;
         case 'nouser':
             message = 'User does not exist.';
             break;
+        case 'user_not_found':
+            message = 'User Not Found';
+            break;
         case 'wrongpwd':
             message = 'Wrong password.';
+            break;
+        case 'password_updated':
+            title = 'Password Updated Successfully.';
+            message = 'You Can Log Now';
+            icon = 'success';
             break;
         case 'success':
             title = 'Login successfully';
@@ -61,12 +82,99 @@ if (stat) {
             message = '';
             icon = 'success';
             break;
+        case 'Member_Details_Deleted':
+            title = 'Member Details Deleted Successfully.';
+            message = '';
+            icon = 'success';
+            break;
+        case 'Book_Deleted':
+            title = 'Book Details Deleted Successfully.';
+            message = '';
+            icon = 'success';
+            break;
+        case 'Book_ID_Already_Exists':
+            title = 'Book ID Already Exists';
+            message = '';
+            break;
+        case 'Invalid_book_id_format':
+            title = 'Invalid Book Id Format';
+            message = '';
+            break;
+
         case 'Updated_successfully':
             title = 'User Account Updated Successfully.';
             message = '';
             icon = 'success';
             break;
+        case 'Book_Reg_Succsseful':
+            title = 'Book Details Added Successfully.';
+            message = '';
+            icon = 'success';
+            break;
+        case 'registration_successful':
+            title = 'Member Details Added Successfully.';
+            message = '';
+            icon = 'success';
+            break;
+        case 'Invalid_member_id_format':
+            title = 'Invalid Member Id Format';
+            message = '';
+            break;
+        case 'Updated_member_successfully':
+            title = 'Member Details Updated Successfully.';
+            message = '';
+            icon = 'success';
+            break;
+        case 'Member_id_already_exists':
+            title = 'Member Id Already Exists';
+            message = '';
+            break;
+        case 'Record_updated_successfully':
+            title = 'Book Borrow Record Updated Successfully';
+            message = '';
+            icon = 'success';
+            break;
+        case 'Error_updating_record':
+            title = 'Error Updating Record';
+            message = '';
+        case 'Book_ID_does_not_exist':
+            title = 'Book ID Does Not Exist';
+            message = '';
+            break;
+        case 'Book_already_borrowed':
+            title = 'Book Already Borrowed';
+            message = 'If You Want To Modify AS "Available". Try Using "View Borrow detail" section and Modify as ""Available".';
+            icon = 'warning';
+            break;
+        case 'Member_ID_does_not_exist':
+            title = 'Member ID Does Not Exist';
+            break;
+        case 'Record_added_successfully':
+            title = 'Book Borrow Details Added Successfully';
+            message = '';
+            icon = 'success';
+            break;
+        case 'Borrow_record_Deleted':
+            title = 'Book Borrow Record Deleted Successfully';
+            message = '';
+            icon = 'success';
+            break;
+        case 'Error_adding_record':
+            title = 'Error Adding Record';
+            break;
+        case 'Invalid_borrow_id_format':
+            title = 'Invalid Borrow Id Format';
+            break;
+        case 'borrow_details_updated_successfully':
+            title = 'Borrow Details Updated Successfully';
+            message = '';
+            icon = 'success';
+            break;
+        case 'Borrow_ID_Already_Exists':
+            title = 'Borrow ID Already Exists';
+            break;
         default:none
+            
         
     }
     swal(title, message, icon);
